3f38c49ec49fd5e9c5882fa5ad3b3850a6f64319
Gustavo Straub <gustavostraubcovers@gmail.com>
2021-02-05 18:00:12 -0300

    express

--- aws.js
+++ aws.js
@@ -1,3 +1,18 @@
+const app = require('express')()
+const cors = require("cors")
+const bodyParser = require("body-parser")
+
+app.use(
+  cors(),
+  bodyParser.json(),
+  bodyParser.urlencoded({ extended: true })
+)
+
+app.listen(4000, () => {
+  console.log("running on port 4000")
+})
+
+
 const aws = require('aws-sdk')
 require('dotenv').config()
 aws.config.update({ region: 'us-east-2' })
@@ -14,36 +29,51 @@ bucketParams = {
   Bucket: 'uploadexamples3',
 }
 
-s3.listObjects(bucketParams, function (err, data) {
-  if (err) {
-    console.log("Error", err)
-  } else {
-    data.Contents.forEach(img => {
-      client.compareFaces({
-        SourceImage: {
-          S3Object: {
-            Bucket: 'uploadexamples3',
-            Name: 'faustao.png'
-          }
-        },
-        TargetImage: {
-          S3Object: {
-            Bucket: "uploadexamples3",
-            Name: img.Key
-          }
-        },
-        SimilarityThreshold: 70
-      }, function (e, r) {
-        if (e) {
-          console.log(e)
-        } else {
-          if (r.FaceMatches.length > 0) {
-            console.log('face found in: ' + 'https://uploadexamples3.s3.us-east-2.amazonaws.com/' + img.Key)
-          } else {
-            console.log('no matches ' )
-          }
-        }
-      })
+let result = []
+function ResultPush(key) { 
+  result.push(key)
+}
+
+app.get('/', async (req, res) => {
+  try {
+    let final
+    s3.listObjects(bucketParams, function (err, data) {
+      if (err) {
+        console.log("Error", err)
+      } else {
+        data.Contents.forEach(img => {
+          client.compareFaces({
+            SourceImage: {
+              S3Object: {
+                Bucket: 'uploadexamples3',
+                Name: 'faustao.png'
+              }
+            },
+            TargetImage: {
+              S3Object: {
+                Bucket: "uploadexamples3",
+                Name: img.Key
+              }
+            },
+            SimilarityThreshold: 70
+          }, function (e, r) {
+            if (e) {
+              console.log(e)
+            } else {
+              if (r.FaceMatches.length > 0) {
+                ResultPush(img.Key)
+                // console.log('face found in: ' + 'https://uploadexamples3.s3.us-east-2.amazonaws.com/' + img.Key)
+              } else {
+                console.log('no matches ' )
+              }
+            }
+          })
+        })
+      }
     })
+    res.send(result)
+  } catch (e) {
+    console.log(e)
   }
+
 })
